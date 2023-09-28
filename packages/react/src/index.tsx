e542c7f33ef52f851759ffd1c7901ae2d6c959e0
Jairo Doni <jairo.doni97@gmail.com>
2023-09-27 21:40:25 -0300

    storybook docs base

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,12 +1,32 @@
 import { styled } from './styles'
 
-const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite500',
-  borderRadius: '$md',
-  height: '$4',
-})
-
-export function App() {
-  return <Button>BATATA</Button>
+export type ButtonProps = {
+  size: 'big' | 'small'
 }
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$md',
+  padding: '$2 $4',
+  border: 0,
+  fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
