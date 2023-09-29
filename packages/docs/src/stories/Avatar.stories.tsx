b0a85a78ee087ad4ef0eb9bbf7281a997066a642
Jairo Doni <jairo.doni97@gmail.com>
2023-09-29 19:27:33 -0300

    display name para componentes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -6,7 +6,7 @@ export default {
   component: Avatar,
   args: {
     src: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80',
-    alt: ' John Smith',
+    alt: 'John Smith',
   },
 } as Meta<AvatarProps>
 
--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -84,3 +84,5 @@ export const Button = styled('button', {
 export interface ButtonProps extends ComponentProps<typeof Button> {
   as?: Element
 }
+
+Button.displayName = 'Button'
